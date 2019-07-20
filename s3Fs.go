5c1140d564ddb45e1b11c2bf49e2552c807c8be1
Jessica Glenn <jessica@skilld.com>
2019-07-21 00:50:25 +1000

    updating to the aws sdk, and starting to add tests in to make sure that this works and can run in production

--- s3Fs.go
+++ s3Fs.go
@@ -1,16 +1,20 @@
-package aferoS3
+package aferos3
 
 import (
+	"bytes"
 	"fmt"
-	"github.com/mitchellh/goamz/aws"
-	"github.com/mitchellh/goamz/s3"
-	"github.com/spf13/afero"
-	"github.com/spf13/afero/mem"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+
 	"io/ioutil"
-	"mime"
 	"os"
 	"strings"
 	"time"
+
+	"github.com/spf13/afero"
+	"github.com/spf13/afero/mem"
 )
 
 /*
@@ -28,47 +32,61 @@ Example:
 
 */
 
+// S3Fs is the base filesystem that we will be sending back to Afero
 type S3Fs struct {
-	Bucket *s3.Bucket
-	auth   *aws.Auth
+	session *session.Session
+	client  *s3.S3
+	bname   string
 }
 
-var USEast = aws.USEast
-var USGovWest = aws.USGovWest
-var USWest = aws.USWest
-var USWest2 = aws.USWest2
-var EUWest = aws.EUWest
-var EUCentral = aws.EUCentral
-var APSoutheast = aws.APSoutheast
-var APSoutheast2 = aws.APSoutheast2
-var APNortheast = aws.APNortheast
-var SAEast = aws.SAEast
-var CNNorth = aws.CNNorth
+// GetBucketFs initialises the FileSystem and then returns an afero.Fs or an error
+// this is how you first initialise S3Fs
+func GetBucketFs(name string, sesh *session.Session) (afero.Fs, error) {
 
-func GetBucket(name string, region aws.Region) (afero.Fs, error) {
-	a, err := aws.EnvAuth()
+	client := s3.New(sesh)
+
+	// Get the ACL of the bucket so that we can test that this bucket actually exists
+	_, err := client.GetBucketAcl(&s3.GetBucketAclInput{
+		Bucket: aws.String(name),
+	})
+
+	if err != nil && strings.Contains(err.Error(), "NoSuchBucket") {
+		fmt.Println(err.Error())
+		_, err = createBucket(client, name)
+	}
+
+	// error should have been overridden if there was no such bucket
 	if err != nil {
 		return nil, err
 	}
 
-	client := s3.New(a, region)
-	bucket := client.Bucket(name)
-
 	return S3Fs{
-		Bucket: bucket,
-		auth:   &a,
+		session: sesh,
+		client:  client,
+		bname:   name,
 	}, nil
 }
 
+// If the bucket that is being passed through doesn't exist then we should be creating it
+func createBucket(client *s3.S3, name string) (string, error) {
+	_, err := client.CreateBucket(&s3.CreateBucketInput{
+		Bucket: aws.String(name),
+	})
+	return name, err
+}
+
+// Name simply returns the filesystem identifier for Afero
 func (S3Fs) Name() string {
 	return "S3Fs"
 }
 
+// Create creates a new file object to be used, this function uses an in memory file
+// at this point and does not communicate with S3 until the file has to be opened
 func (S3Fs) Create(name string) (afero.File, error) {
-	return mem.CreateFile(name), nil
+	return mem.NewFileHandle(mem.CreateFile(name)), nil
 }
 
-// Read from s3, and bring down whole file? or torrent?
+// Open will read from s3, and bring down whole file? or torrent?
 func (s S3Fs) Open(name string) (afero.File, error) {
 
 	memFile, err := s.Create(getNameFromPath(name))
@@ -76,23 +94,36 @@ func (s S3Fs) Open(name string) (afero.File, error) {
 		return nil, err
 	}
 
-	torrent, err := s.Bucket.GetTorrent(name)
+	res, err := s.client.GetObject(&s3.GetObjectInput{
+		Bucket: aws.String(s.bname),
+		Key:    aws.String(name),
+	})
 	if err != nil {
 		return memFile, err
 	}
 
-	memFile.Write(torrent)
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return memFile, err
+	}
+
+	memFile.Write(b)
 
 	return memFile, err
 }
 
+// Push executes a Put to the S3 bucket
 func (s S3Fs) Push(f afero.File, path string) error {
 
 	body, err := ioutil.ReadAll(f)
-
 	if err != nil {
-		s.Bucket.Put(path, body, mime.TypeByExtension(path), "")
+		return err
 	}
+	s.client.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(s.bname),
+		Key:    aws.String(path),
+		Body:   bytes.NewReader(body),
+	})
 
 	return err
 }
@@ -111,43 +142,103 @@ func getNameFromPath(fileName string) string {
 	return fmt.Sprintf("%s.%s", name, ext)
 }
 
+// S3FileInfo gives file information for a file in S3
 type S3FileInfo struct {
 	os.FileInfo
 	file *afero.File
 }
 
-// Maybe different between Open is its torrent, and this is the actuall file
+// OpenFile returns the same thing as Open for the moment, this may change later
 func (s S3Fs) OpenFile(name string, flag int, perm os.FileMode) (afero.File, error) {
 	file, err := s.Open(name)
 	s.Chmod(name, perm)
 	return file, err
 }
 
-// Set ACL Perms
+// Chmod doesn't do anything at the moment as it may have unintended consequences
+// however it needs to be here to comply with Afero
 func (s S3Fs) Chmod(name string, mode os.FileMode) error {
 	return nil
 }
 
+// Chtimes does absolutely nothing yet
 func (S3Fs) Chtimes(name string, atime time.Time, mtime time.Time) error {
 	return nil
 }
 
+// Stat provides very light file information, this needs to be augmented later
 func (s S3Fs) Stat(name string) (os.FileInfo, error) {
 	f, err := s.Open(name)
 	return S3FileInfo{file: &f}, err
 }
 
-// Renames a file
+// Rename creates a copy of the file with the new name and then deletes the old file
 func (s S3Fs) Rename(oldname, newname string) error {
-	return s.Bucket.Copy(oldname, newname, s3.ACL(""))
+	// Create a new copy
+	// Delete the old file
+	_, err := s.client.CopyObject(&s3.CopyObjectInput{
+		CopySource: aws.String(fmt.Sprintf("%s/%s", s.bname, oldname)),
+		Bucket:     aws.String(s.bname),
+		Key:        aws.String(newname),
+	})
+	if err != nil {
+		return err
+	}
+
+	return s.Remove(oldname)
 }
 
-// Removes a file
+// Remove deletes the file from S3
 func (s S3Fs) Remove(name string) error {
-	return s.Bucket.Del(name)
+	_, err := s.client.DeleteObject(&s3.DeleteObjectInput{
+		Bucket: aws.String(s.bname),
+		Key:    aws.String(name),
+	})
+	return err
 }
 
-// Dont think we can do much here
-func (S3Fs) Mkdir(name string, perm os.FileMode) error    { return nil }
-func (S3Fs) MkdirAll(path string, perm os.FileMode) error { return nil }
-func (S3Fs) RemoveAll(path string) error                  { return nil }
+// Mkdir does nothing at the moment
+func (s S3Fs) Mkdir(name string, perm os.FileMode) error { return nil }
+
+// MkdirAll also does nothing as S3 doesn't really do diretory struture
+func (s S3Fs) MkdirAll(path string, perm os.FileMode) error { return nil }
+
+// RemoveAll lists all files in a path and then removes them all one by one
+func (s S3Fs) RemoveAll(path string) error {
+
+	// list all of the files for this prefix
+	keys := make([]string, 0)
+	res, err := s.client.ListObjects(&s3.ListObjectsInput{
+		Bucket: aws.String(s.bname),
+		Prefix: aws.String(path),
+	})
+	if err != nil {
+		return err
+	}
+	for _, obj := range res.Contents {
+		keys = append(keys, *(obj.Key))
+	}
+	for res.NextMarker != nil {
+		res, err = s.client.ListObjects(&s3.ListObjectsInput{
+			Bucket: aws.String(s.bname),
+			Prefix: aws.String(path),
+			Marker: res.NextMarker,
+		})
+		if err != nil {
+			return err
+		}
+		for _, obj := range res.Contents {
+			keys = append(keys, *(obj.Key))
+		}
+	}
+
+	// delete each of them
+	for _, key := range keys {
+		err = s.Remove(key)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
--- s3Fs_test.go
+++ s3Fs_test.go
@@ -1,27 +1,68 @@
-package aferoS3
+package aferos3
 
 import (
-	"github.com/mitchellh/goamz/aws"
-	"github.com/mitchellh/goamz/s3"
-	"github.com/spf13/afero"
+	"bytes"
+	"io"
+	"os"
 	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/spf13/afero"
 )
 
-func TestS3OpenFile(t *testing.T) {
+const BucketName = "testbucket.s3fs"
+const TestKey = "a/test/key.ext"
+const TestObject = "This is some random text to go in here"
 
-	auth, err := aws.EnvAuth()
+func TestMain(m *testing.M) {
+	sesh := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1"),
+	}))
+	client := s3.New(sesh)
+
+	// Create the bucket
+	_, err := client.CreateBucket(&s3.CreateBucketInput{
+		Bucket: aws.String(BucketName),
+	})
+
 	if err != nil {
-		t.Fatal(err)
+		os.Exit(1)
 	}
 
-	client := s3.New(auth, aws.USEast)
-	bucket := client.Bucket("bucket_name")
+	// seed the bucket with the test object
+	client.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(BucketName),
+		Key:    aws.String(TestKey),
+		Body:   bytes.NewReader([]byte(TestObject)),
+	})
 
-	var AppFs afero.Fs = S3Fs{
-		Bucket: bucket,
+	m.Run()
+
+	// remove the seeded test object
+	client.DeleteObject(&s3.DeleteObjectInput{
+		Bucket: aws.String(BucketName),
+		Key:    aws.String(TestKey),
+	})
+
+	// Delete the bucket
+	client.DeleteBucket(&s3.DeleteBucketInput{
+		Bucket: aws.String(BucketName),
+	})
+}
+
+func TestOpenFile(t *testing.T) {
+
+	sesh := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1"),
+	}))
+	sfs, err := GetBucketFs(BucketName, sesh)
+	if err != nil {
+		t.Error(err)
 	}
 
-	file, err := AppFs.Open("path/to/file")
+	file, err := sfs.Open(TestKey)
 
 	if err != nil {
 		t.Error(err)
@@ -34,4 +75,30 @@ func TestS3OpenFile(t *testing.T) {
 	var OsFs afero.Fs = afero.OsFs{}
 	newFile, err := OsFs.Create("output.jpg")
 	io.Copy(newFile, file)
+}
+
+func TestNewFile(t *testing.T) {
+	// create a new file through the system
+	// check that it's there
+	// delete the file to clean it up
+}
+
+func TestRenameFile(t *testing.T) {
+	// create a file
+	// make sure that it's there
+	// rename the file
+	// check that the new file is there
+	// delete the new filename to make sure it's clean
+}
+
+func TestRemoveFile(t *testing.T) {
+	// create a file
+	// delete the file
+	// remove the file
+}
+
+func TestRemoveAll(t *testing.T) {
+	// seed a multitude of files in a multitude of paths
+	// check that they're there
+	// then try deleting them with the remove all function
 }
